82c4176299ad31ae4081dd70ba3740b35d902351
Eric Fiselier <eric@efcs.ca>
2017-05-03 21:40:50 -0600

    attempt to get newer clang working

--- install-clang.cmd
+++ install-clang.cmd
@@ -1,11 +1,12 @@
 @echo on
 if "%CLANG_VERSION%"=="ToT" (
     if NOT EXIST install-llvm (
-      appveyor DownloadFile http://efcs.ca/downloads/llvm-tot-win32.zip -FileName llvm-package.zip
+      if NOT EXIST llvm-package.zip (
+        appveyor DownloadFile http://efcs.ca/downloads/llvm-tot-win32.zip -FileName llvm-package.zip
+      )
       7z x llvm-package.zip -oC:\projects\deps
     )
     move "C:\Program Files\LLVM" "C:\Program Files\LLVM_BAK"
     mklink /D "C:\Program Files\LLVM" "C:\projects\deps\install-llvm"
     set LLVM_PATH="C:\projects\deps\install-llvm"
-    dir "C:\Program Files\LLVM"
 )
