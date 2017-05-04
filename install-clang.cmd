a159c386319f5445f2945190120d30792ba14ea2
Eric Fiselier <eric@efcs.ca>
2017-05-03 20:29:00 -0600

    fix install-clang.cmd

--- install-clang.cmd
+++ install-clang.cmd
@@ -1,5 +1,5 @@
 if "%CLANG_VERSION%"=="ToT" (
-    CLANG_URL=http://llvm.org/pre-releases/win-snapshots/LLVM-5.0.0-r301646-win32.exe
+    set CLANG_URL=http://llvm.org/pre-releases/win-snapshots/LLVM-5.0.0-r301646-win32.exe
     appveyor DownloadFile %CLANG_URL% -FileName clang-installer.exe
     clang-installer.exe
 )
