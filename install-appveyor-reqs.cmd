4b96c3d3b9bf8cb542956bd5a0b8bac156bbfa8a
Eric Fiselier <eric@efcs.ca>
2017-05-03 23:16:49 -0600

    restash

--- install-appveyor-reqs.cmd
+++ install-appveyor-reqs.cmd
@@ -7,10 +7,8 @@ cd C:\projects\deps
 ::###########################################################################
 :: Setup the path to Clang-cl
 ::###########################################################################
-if NOT EXIST llvm-package.zip (
-    appveyor DownloadFile http://efcs.ca/downloads/llvm-tot-win32.zip -FileName llvm-package.zip
-)
 if "%CLANG_VERSION%"=="ToT" (
+    appveyor DownloadFile http://efcs.ca/downloads/llvm-tot-win32.zip -FileName llvm-package.zip
     move "C:\Program Files\LLVM" "C:\Program Files\LLVM_BAK"
     7z x llvm-package.zip -o"C:\Program Files\LLVM" > nul
 )
