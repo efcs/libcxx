409827f6e5965816235c369270eeca71aa18616f
Eric Fiselier <eric@efcs.ca>
2017-05-04 10:05:27 -0600

    don't break LLVM first

--- install-appveyor-reqs.cmd
+++ install-appveyor-reqs.cmd
@@ -10,7 +10,7 @@ cd C:\projects\deps
 if "%CLANG_VERSION%"=="ToT" (
     #appveyor DownloadFile http://efcs.ca/downloads/llvm-tot-win32.zip -FileName llvm-package.zip
     appveyor DownloadFile http://llvm.org/pre-releases/win-snapshots/LLVM-5.0.0-r301646-win32.exe -FileName llvm-installer.exe
-    move "C:\Program Files\LLVM" "C:\Program Files\LLVM_BAK"
+    #move "C:\Program Files\LLVM" "C:\Program Files\LLVM_BAK"
     llvm-installer.exe /S /D="C:\Program Files\LLVM"
     #7z x llvm-package.zip -o"C:\Program Files\LLVM" > nul
     #rm llvm-package.zip
