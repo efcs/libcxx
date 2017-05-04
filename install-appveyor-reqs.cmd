f218c3a503cadbbb7d1b906fabb195e7fd2b4eb3
Eric Fiselier <eric@efcs.ca>
2017-05-03 22:33:48 -0600

    cleanup

--- install-appveyor-reqs.cmd
+++ install-appveyor-reqs.cmd
@@ -11,9 +11,8 @@ if NOT EXIST llvm-package.zip (
     appveyor DownloadFile http://efcs.ca/downloads/llvm-tot-win32.zip -FileName llvm-package.zip
 )
 if "%CLANG_VERSION%"=="ToT" (
-    7z x llvm-package.zip -oC:\projects\deps > nul
     move "C:\Program Files\LLVM" "C:\Program Files\LLVM_BAK"
-    mklink /D "C:\Program Files\LLVM" "C:\projects\deps\install-llvm"
+    7z x llvm-package.zip -o"C:\Program Files\LLVM" > nul
 )
 set PATH="C:\Program Files\LLVM\bin";%PATH%
 clang-cl -v
@@ -25,6 +24,7 @@ if NOT EXIST cmake (
   appveyor DownloadFile https://cmake.org/files/v3.7/cmake-3.7.2-win64-x64.zip -FileName cmake.zip
   7z x cmake.zip -oC:\projects\deps > nul
   move C:\projects\deps\cmake-* C:\projects\deps\cmake
+  rm cmake.zip
 )
 set PATH=C:\projects\deps\cmake\bin;%PATH%
 cmake --version
@@ -35,6 +35,7 @@ cmake --version
 if NOT EXIST ninja (
   appveyor DownloadFile https://github.com/ninja-build/ninja/releases/download/v1.6.0/ninja-win.zip -FileName ninja.zip
   7z x ninja.zip -oC:\projects\deps\ninja > nul
+  rm ninja.zip
 )
 set PATH=C:\projects\deps\ninja;%PATH%
 ninja --version
