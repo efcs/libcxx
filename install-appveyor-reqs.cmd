5c572bbf1255a01d3af3f1338d97ed9b577e178f
Eric Fiselier <eric@efcs.ca>
2017-05-03 22:06:51 -0600

    attempt to fix path

--- install-appveyor-reqs.cmd
+++ install-appveyor-reqs.cmd
@@ -8,10 +8,10 @@ cd C:\projects\deps
 :: Setup the path to Clang-cl
 ::###########################################################################
 if "%CLANG_VERSION%"=="ToT" (
-    if NOT EXIST llvm-package.zip (
+    if NOT EXIST install-llvm (
       appveyor DownloadFile http://efcs.ca/downloads/llvm-tot-win32.zip -FileName llvm-package.zip
+      7z x llvm-package.zip -oC:\projects\deps
     )
-    7z x llvm-package.zip -oC:\projects\deps
     move "C:\Program Files\LLVM" "C:\Program Files\LLVM_BAK"
     mklink /D "C:\Program Files\LLVM" "C:\projects\deps\install-llvm"
     set LLVM_PATH="C:\projects\deps\install-llvm"
