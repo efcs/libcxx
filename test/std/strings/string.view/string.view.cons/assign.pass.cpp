8022d96acc74e41709bb922c94e0255cb96530e2
Eric Fiselier <eric@efcs.ca>
2017-10-02 23:35:08 -0600

    add fix and tests

--- test/std/strings/string.view/string.view.cons/assign.pass.cpp
+++ test/std/strings/string.view/string.view.cons/assign.pass.cpp
@@ -7,7 +7,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-
 // <string_view>
 
 // constexpr basic_string_view& operator=(const basic_string_view &) noexcept = default;
@@ -17,34 +16,45 @@
 
 #include "test_macros.h"
 
-template<typename T>
+template <typename T>
 #if TEST_STD_VER > 11
 constexpr
 #endif
-bool test (T sv0)
-    {
-    T sv1;
-    sv1 = sv0;
-//  We can't just say "sv0 == sv1" here because string_view::compare
-//  isn't constexpr until C++17, and we want to support back to C++14
-    return sv0.size() == sv1.size() && sv0.data() == sv1.data();
-    }
+    bool
+    test(T sv0) {
+  T sv1;
+  sv1 = sv0;
+  //  We can't just say "sv0 == sv1" here because string_view::compare
+  //  isn't constexpr until C++17, and we want to support back to C++14
+  return sv0.size() == sv1.size() && sv0.data() == sv1.data();
+}
 
-int main () {
-
-    assert( test<std::string_view>    ( "1234"));
-#ifdef _LIBCPP_HAS_NO_UNICODE_CHARS
-    assert( test<std::u16string_view> (u"1234"));
-    assert( test<std::u32string_view> (U"1234"));
+void test_assign_rvalue() {
+#if TEST_STD_VER >= 11
+  static_assert(!std::is_assignable<std::string_view, std::string&&>::value, "");
+  static_assert(!std::is_assignable<std::string_view, const std::string&&>::value, "");
+  static_assert(!std::is_assignable<std::string_view, std::string>::value, "");
+  static_assert(!std::is_assignable<std::string_view, const std::string>::value, "");
 #endif
-    assert( test<std::wstring_view>   (L"1234"));
+}
+
+int main() {
+
+  assert(test<std::string_view>("1234"));
+#ifdef _LIBCPP_HAS_NO_UNICODE_CHARS
+  assert(test<std::u16string_view>(u "1234"));
+  assert(test<std::u32string_view>(U "1234"));
+#endif
+  assert(test<std::wstring_view>(L"1234"));
 
 #if TEST_STD_VER > 11
-    static_assert( test<std::string_view>    ({ "abc", 3}), "");
+  static_assert(test<std::string_view>({"abc", 3}), "");
 #ifdef _LIBCPP_HAS_NO_UNICODE_CHARS
-    static_assert( test<std::u16string_view> ({u"abc", 3}), "");
-    static_assert( test<std::u32string_view> ({U"abc", 3}), "");
+  static_assert(test<std::u16string_view>({u "abc", 3}), "");
+  static_assert(test<std::u32string_view>({U "abc", 3}), "");
 #endif
-    static_assert( test<std::wstring_view>   ({L"abc", 3}), "");
+  static_assert(test<std::wstring_view>({L"abc", 3}), "");
 #endif
+
+  test_assign_rvalue();
 }
