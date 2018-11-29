baaf0b310fd97d4ddfe11a504e40bbc61cf6845e
Eric Fiselier <eric@efcs.ca>
2018-11-29 15:31:16 -0500

    dummy

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,8 +1,12 @@
 #include <__tree>
+#include <map>
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 template struct __tree_operations<void*>;
 template class __tree<string, less<string>, allocator<string> >;
+template class __tree<_MapVT,
+                             __map_value_compare<string, _MapVT, less<string> >,
+                             allocator<_MapVT> >;
 
 _LIBCPP_END_NAMESPACE_STD
