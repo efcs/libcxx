0331f3a5c0023de1e12f12928cdedb51d93181f8
Eric Fiselier <eric@efcs.ca>
2018-11-29 22:49:44 -0500

    dummy

--- src/tree.cpp
+++ src/tree.cpp
@@ -6,10 +6,8 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 template struct __tree_operations<void*>;
 
 _LIBCPP_TREE_INSTANT( );
+_LIBCPP_MAP_INSTANTS( )
 
-template class __tree<_MapVT,
-                             __map_value_compare<string, _MapVT, less<string> >,
-                             allocator<_MapVT> >;
 template
 pair<__tree_iterator<string, __tree_node<string, void*>*, long>, bool>
 __tree<string, less<string >, allocator<string > >::__emplace_unique_key_args<string, string const&>(string const&, string const&);
