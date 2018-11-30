ec317545dc5867ff98567fa4357a40ea6e4e9b03
Eric Fiselier <eric@efcs.ca>
2018-11-29 19:48:05 -0500

    dummy

--- src/tree.cpp
+++ src/tree.cpp
@@ -15,4 +15,11 @@ pair<__tree_iterator<string, __tree_node<string, void*>*, long>, bool>
 __tree<string, less<string >, allocator<string > >::__emplace_unique_key_args<string, string const&>(string const&, string const&);
 
 
+template class __tree<_MapVT,
+                             __map_value_compare<string, _MapVT, less<string> >,
+                             allocator<_MapVT> >;
+template
+pair<__tree_iterator<string, __tree_node<string, void*>*, long>, bool>
+__tree<string, less<string >, allocator<string > >::__emplace_unique_key_args<string, string const&>(string const&, string const&);
+
 _LIBCPP_END_NAMESPACE_STD
