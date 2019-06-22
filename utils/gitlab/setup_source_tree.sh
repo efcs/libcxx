37d318c0b1b63fd0e82255ee905406f4c7cf39c8
Eric Fiselier <eric@efcs.ca>
2019-06-22 18:47:28 -0400

    changes

--- utils/gitlab/setup_source_tree.sh
+++ utils/gitlab/setup_source_tree.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-$
+set -x
+set -e
+
+pwd
+mkdir -p /tmp/$CI_PROJECT_PATH
+mv -t /tmp/$CI_PROJECT_PATH/ *
+ls
+mkdir build/ stage/ ci/
+mv /tmp/$CI_PROJECT_NAMESPACE ci/
+git clone --depth=1 https://github.com/llvm/llvm-project.git llvm-project/
+touch build/BUILD_HERE
+touch stage/STAGE_HERE
+ls
