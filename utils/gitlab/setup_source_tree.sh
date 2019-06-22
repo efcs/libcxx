7ea19f4b5044dae48f3d7a6855fa4afb34cbb02d
Eric Fiselier <eric@efcs.ca>
2019-06-22 19:11:20 -0400

    changes

--- utils/gitlab/setup_source_tree.sh
+++ utils/gitlab/setup_source_tree.sh
@@ -3,13 +3,14 @@
 set -x
 set -e
 
-pwd
+echo "Reconfiguring source layout for $CI_PROJECT_DIR"
+
+pushd $CI_PROJECT_DIR
 mkdir -p /tmp/$CI_PROJECT_PATH
 mv -t /tmp/$CI_PROJECT_PATH/ *
-ls
 mkdir build/ stage/ ci/
 mv /tmp/$CI_PROJECT_NAMESPACE ci/
 git clone --depth=1 https://github.com/llvm/llvm-project.git llvm-project/
 touch build/BUILD_HERE
 touch stage/STAGE_HERE
-ls
+popd
