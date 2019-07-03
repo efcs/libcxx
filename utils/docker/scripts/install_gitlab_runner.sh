140ada631e77a5391d6fd11e229cc0eaf261e65f
Eric Fiselier <eric@efcs.ca>
2019-07-03 18:08:37 -0400

    stuff

--- utils/docker/scripts/install_gitlab_runner.sh
+++ utils/docker/scripts/install_gitlab_runner.sh
@@ -8,12 +8,3 @@ chmod +x /usr/local/bin/gitlab-runner
 useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 
 # Create the directory used for the gitlab runner config
-
-if [[ -z "$GITLAB_CONFIG_VOLUME" ]]; then
-  echo "Error, GITLAB_CONFIG_VOLUME must be defined"
-  exit 1
-fi
-
-mkdir $GITLAB_CONFIG_VOLUME
-
-cp gitlab/config.toml $GITLAB_CONFIG_VOLUME/config.toml
