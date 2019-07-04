f25dcad1146d41e33e961f0e7f5afbaf52678c6c
Eric Fiselier <eric@efcs.ca>
2019-07-03 20:30:07 -0400

    changes

--- utils/docker/gitlab/gcloud/scripts/register_and_run_gitlab_runner.sh
+++ utils/docker/gitlab/gcloud/scripts/register_and_run_gitlab_runner.sh
@@ -21,7 +21,7 @@ export PATH=$PATH:$GITLAB_SCRIPTS/
 
 readonly TOKEN=$(cat /run/secrets/llvm_gitlab_token)
 
-docker run  --rm \
+docker run  -d --restart always \
    -v $GITLAB_CONFIG_VOLUME:/etc/gitlab-runner \
    -v /var/run/docker.sock:/var/run/docker.sock \
    gitlab/gitlab-runner register \
