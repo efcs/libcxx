e0a65d493906a44d409ee017518d10e69b6a5838
Eric Fiselier <eric@efcs.ca>
2019-09-13 15:16:01 -0400

    cleanup

--- utils/docker/debian9/gitlab/install_gitlab_runner.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
-chmod +x /usr/local/bin/gitlab-runner
-useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
