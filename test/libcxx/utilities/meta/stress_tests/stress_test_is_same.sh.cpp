5036e128079c0e469c1625c36b64327937e94373
Eric Fiselier <eric@efcs.ca>
2019-09-20 16:39:49 -0600

    changes

--- test/libcxx/utilities/meta/stress_tests/stress_test_is_same.sh.cpp
+++ test/libcxx/utilities/meta/stress_tests/stress_test_is_same.sh.cpp
@@ -29,7 +29,7 @@
 template <int N> struct Arg { enum { value = 1 }; };
 
 
-#define TYPE_LIST_ORIG \
+#define TYPE_LIST \
   int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, \
   int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, \
   int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, \
@@ -75,31 +75,24 @@ template <int N> struct Arg { enum { value = 1 }; };
   int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, \
   int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, \
   int, int, int, int, int, int, int, int, int, int, int, int, int, int, int
-#define TYPE_LIST TYPE_LIST_ORIG
-#define INT_TYPE_() int,
+
 
 
 #ifdef TEST_NEW
 #define TUPLE_ELEMENT(I)  __type_pack_element<I,  TYPE_LIST>
 #else
-#define TUPLE_ELEMENT(I) std::tuple_element_t<I, std::tuple< TYPE_LIST> >
+#define TUPLE_ELEMENT(I) typename std::tuple_element<I,  std::tuple< TYPE_LIST> >::type
 #endif
 
 #define TEST_CASE() TUPLE_ELEMENT(__COUNTER__){},
 
-int sink(...) { volatile int v = 0; return v;  }
+#define TEST_CASE_TWO() TUPLE_ELEMENT(__COUNTER__),
+
+int sink(REPEAT_100(TEST_CASE_TWO) int) { volatile int v = 0; return v;  }
+
 int x = sink(
   REPEAT_100(TEST_CASE) int{}
 );
-
-#undef TEST_CASE
-#undef TYPE_LIST
-#define TYPE_LIST T...
-#define TEST_CASE() TUPLE_ELEMENT(__COUNTER__),
-
-template <class ...T>
-void Foo(REPEAT_100(TEST_CASE) int) {}
-template void Foo<TYPE_LIST_ORIG>(REPEAT_100(TUPLE_ELEMENT(__COUNTER__)), int);
 
 
 int main() {}
